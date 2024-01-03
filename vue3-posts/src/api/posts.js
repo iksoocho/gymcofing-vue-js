94d1c3fad9f74f00012615974e0ab8180962205f
iksoocho <jogaeya@gmail.com>
2024-01-03 18:47:57 +0900

    0103

--- vue3-posts/src/api/posts.js
+++ vue3-posts/src/api/posts.js
@@ -8,8 +8,8 @@ const posts = [
   { id: 5, title: '제목5', content: '내용5', createAt: '2025-05-05' },
 ];
 
-export function getPosts() {
-  return axios.get('http://localhost:5000/posts');
+export function getPosts(params) {
+  return axios.get('http://localhost:5000/posts', { params });
 }
 
 export function getPostById(id) {
--- vue3-posts/src/views/posts/PostListView.vue
+++ vue3-posts/src/views/posts/PostListView.vue
@@ -12,7 +12,25 @@
         ></PostItem>
       </div>
     </div>
-    <hr class="my-4" />
+    <nav class="mt-5" aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <!--justify-content-center > pagination을 화면 중앙으로 오게 하기 위해-->
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <hr class="my-5" />
     <AppCard>
       <PostDetailView :id="2"></PostDetailView>
     </AppCard>
@@ -29,10 +47,15 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const posts = ref([]);
+const params = ref({
+  _sort: 'createAt',
+  _order: 'desc',
+  _limit: 3,
+});
 
 const fetchPosts = async () => {
   try {
-    const { data } = await getPosts();
+    const { data } = await getPosts(params.value);
     posts.value = data;
   } catch (error) {
     console.error(error);
